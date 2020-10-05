087bff04f6aab4fcc1f6e3c14371bd90535694d7
ber01 <ber01@naver.com>
2020-10-06 00:04:06 +0900

    Updated AccountAdapter
    - 세션 로그인, 소셜 로그인 둘 다 작동되게 수정

--- src/main/java/com/kyunghwan/loginskeleton/auth/OAuth2UserServiceImpl.java
+++ src/main/java/com/kyunghwan/loginskeleton/auth/OAuth2UserServiceImpl.java
@@ -1,27 +1,22 @@
 package com.kyunghwan.loginskeleton.auth;
 
 import com.kyunghwan.loginskeleton.account.Account;
+import com.kyunghwan.loginskeleton.account.AccountAdapter;
 import com.kyunghwan.loginskeleton.account.AccountRepository;
 import com.kyunghwan.loginskeleton.auth.dto.OAuthAttributes;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
-
-import javax.servlet.http.HttpSession;
-import java.util.Collections;
 
 @RequiredArgsConstructor
 @Service
 public class OAuth2UserServiceImpl implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
     private final AccountRepository accountRepository;
-    private final HttpSession httpSession;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -35,9 +30,7 @@ public class OAuth2UserServiceImpl implements OAuth2UserService<OAuth2UserReques
         OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
 
         Account account = saveOrUpdate(attributes);
-        httpSession.setAttribute("user", new SessionAccount(account));
-
-        return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority(account.getRoleKey())), attributes.getAttributes(), attributes.getNameAttributeKey());
+        return new AccountAdapter(account, attributes);
     }
 
     private Account saveOrUpdate(OAuthAttributes attributes) {
